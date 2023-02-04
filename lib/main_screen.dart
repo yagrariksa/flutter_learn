3a0ac51510236d7659730d353f836f4749f0c713
yagrariksa <luckyrama0@gmail.com>
2023-02-04 21:04:07 +0700

    data prep

--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -1,6 +1,24 @@
+import 'dart:math';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_learn/models/note.dart';
 import 'package:flutter_learn/utils/font.dart';
+import 'package:flutter_learn/utils/note_colors.dart';
+import 'package:flutter_lorem/flutter_lorem.dart';
+
+final mockNote = Iterable.generate(
+  100,
+  (index) => Note(
+      id: index,
+      title: "Title #${index + 1}",
+      content: lorem(
+        paragraphs: Random().nextInt(3),
+        words: Random().nextInt(100),
+      ),
+      noteColor:
+          NoteColors.keys.elementAt(Random().nextInt(NoteColors.length))),
+);
 
 class ListNotePage extends StatelessWidget {
   const ListNotePage({Key? key}) : super(key: key);
---
+++ lib/models/note_database.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_learn/models/note.dart';
+import 'package:sqflite/sqflite.dart';
+
+class NoteDatabase {
+  static final _name = "NotesDatabase.db";
+  static final _version = 1;
+
+  late Database database;
+  static final tableName = "notes";
+
+  initDatabase() async {
+    database = await openDatabase(
+      _name,
+      version: _version,
+      onCreate: (Database db, int version) async {
+        await db.execute(
+          '''CREATE TABLE $tableName (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            title TEXT,
+            content TEXT,
+            noteColor TEXT
+          )
+          ''',
+        );
+      },
+    );
+  }
+
+  Future<int> inserNote(Note note) async {
+    return await database.insert(
+      tableName,
+      note.toMap(),
+      conflictAlgorithm: ConflictAlgorithm.replace,
+    );
+  }
+
+  Future<int> updateNote(Note note) async {
+    return await database.update(
+      tableName,
+      note.toMap(),
+      where: "id = ?",
+      whereArgs: [note.id],
+      conflictAlgorithm: ConflictAlgorithm.replace,
+    );
+  }
+
+  Future<List<Map<String, dynamic>>> getAllNotes() async {
+    return await database.query(tableName);
+  }
+
+  Future<Map<String, dynamic>?> getNote(int id) async {
+    var result = await database.query(
+      tableName,
+      where: "id = ?",
+      whereArgs: [id],
+    );
+
+    if (result.length > 0) {
+      return result.first;
+    }
+
+    return null;
+  }
+
+  Future<int> deleteNote(int id) async {
+    return await database.delete(
+      tableName,
+      where: "id = ?",
+      whereArgs: [id],
+    );
+  }
+
+  closeDatabase() async {
+    await database.close();
+  }
+}
